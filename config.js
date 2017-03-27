705074d89dc58f432d010eecee592ba8924d2da4
James <jamescat29@gmail.com>
2017-03-26 23:28:25 -0500

    first commit

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+	host: 'services.groupkt.com',
+	//port:80,
+	path: '/country/get/iso2code/PE',
+	method: 'GET'
+}
---
+++ index.js
@@ -0,0 +1,50 @@
+'use strict'
+
+const http = require('./app')
+const options = require('./config')
+var cron = require('node-cron');
+
+
+function obtenerPais(options,cb){
+
+	http.request(options,(res)=>{
+		var body = '';
+
+		res.on('data',(chunck)=>{
+			body+= chunck;
+		});
+
+		res.on('end',()=>{
+			var obj = JSON.parse(body);
+			cb(null,obj);
+		});
+
+		res.on('error',cb);
+
+	})
+	.on('error',cb)
+	.end();
+}
+
+/*
+var task = cron.schedule('* * * * *', function(){
+    /*obtenerPais(options,(err,obj)=>{
+		if(err){
+			return console.log('Error durante la llamada al servicio',err);
+		}
+		console.log(obj);
+	});
+	console.log('ejecutado inmediato');
+});
+
+task.stop();
+*/
+cron.schedule('* * * * *', function(){
+	obtenerPais(options,(err,obj)=>{
+		if(err){
+			return console.log('Error durante la llamada al servicio',err);
+		}
+		console.log(obj);
+	});
+  console.log('running a task every minute');
+});
